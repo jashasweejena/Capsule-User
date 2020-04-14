31eac54ee282421220fee4758a5ea6119abe5a5e
Bhavnaharitsa <bhavnaharitsa@gmail.com>
2020-04-14 09:13:21 +0530

    added splash screen, intro screens and phone otp authentication

--- app/src/main/java/com/example/capsule_user/MainActivity.java
+++ app/src/main/java/com/example/capsule_user/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.capsule_user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int splash_screen_timer = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, DescActivity.class);
+                startActivity(intent);
+            }
+        }, splash_screen_timer);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
 
     }
